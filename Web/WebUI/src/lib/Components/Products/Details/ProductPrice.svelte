5a15d18046c016fe9502e6225889fe71fe82b1c0
Jakub Piasecki <jakubpiasecki05@gmail.com>
2022-08-25 09:17:43 +0200

    Details page's starting to look better finally Added better price and name design Fixed a slider bug with scroll

--- Web/WebUI/src/lib/Components/Products/Details/ProductPrice.svelte
+++ Web/WebUI/src/lib/Components/Products/Details/ProductPrice.svelte
@@ -1,19 +1,44 @@
 <script lang="ts">
 import type Product from "src/lib/Models/product";
-
+import BuyButtons from "./BuyButtons.svelte";
+import QuantityInput from "./QuantityInput.svelte";
 
     export let product: Product;
 </script>
 
 <div class="container">
-    <h1>{product.name}</h1>
-    <h2>{product.price}</h2>
+    <span class="h1">
+        <h1>{product.name}</h1>
+    </span>
+    <span class="h2">
+        <h2>{product.price}$</h2>
+    </span>
+    <BuyButtons />
+    <QuantityInput />
 </div>
 
 <style>
+    @import url('https://fonts.googleapis.com/css2?family=Raleway&display=swap');
     .container {
-        background-color: white;
+        
         grid-area: price;
-        /* height: 20vh; */
+        display: grid;
+        grid-template-columns: 1fr 25%;
+        grid-template-rows: 50% 1fr;
+    }
+    .h1 {
+        font-family: 'Rubik';
+        font-size: 4rem;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    .h2 {
+        background-color: aliceblue;
+        font-family: 'Rubik';
+        font-size: 4.5rem;
+        display: flex;
+        align-items: center;
+        justify-content:center;
     }
 </style>
