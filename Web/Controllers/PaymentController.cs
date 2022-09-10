5c59773cea3d0d324d0b510f6de850d6f41ea3ed
Jakub Piasecki <jakubpiasecki05@gmail.com>
2022-09-10 12:59:22 +0200

    going for braintree rather than stripe

--- Web/Controllers/PaymentController.cs
+++ Web/Controllers/PaymentController.cs
@@ -1,8 +1,16 @@
 using Microsoft.AspNetCore.Mvc;
+using Stripe;
+using Stripe.Checkout;
 
 namespace Web.Controllers;
 
+[ApiController]
+[Route("api/[controller]")]
 public class PaymentController : ControllerBase
 {
-    
+    [HttpGet("charge")]
+    public async Task<IActionResult> Test()
+    {
+        return Ok();
+    }
 }
