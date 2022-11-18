45b9fc5ff4efb5ccbd119027038882303d05d778
Jakub Piasecki <jakubpiasecki05@gmail.com>
2022-11-18 15:14:40 +0100

    ShoppingCart entity changed so it has less properties

--- Infrastructure/Validation/CartProductValidator.cs
+++ Infrastructure/Validation/CartProductValidator.cs
@@ -8,5 +8,6 @@ public class CartProductValidator : AbstractValidator<CartProduct>
     public CartProductValidator()
     {
         RuleFor(x => x.ProductQuantity).GreaterThan(0);
+        RuleFor(x => x.Product).NotNull().WithMessage("Invalid product ID");
     }
 }
